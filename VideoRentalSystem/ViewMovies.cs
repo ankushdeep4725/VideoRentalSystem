76788190f7a3a829a1648e6698e7cdbf9e00632f
Ankushdeep Singh <singhda19977@gmail.com>
2018-08-21 17:02:50 +0530

    Skeleton of Application created.

--- VideoRentalSystem/ViewMovies.cs
+++ VideoRentalSystem/ViewMovies.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace VideoRentalSystem
 {
-    public partial class Form1 : Form
+    public partial class ViewMovies : Form
     {
-        public Form1()
+        public ViewMovies()
         {
             InitializeComponent();
         }
